fd418b76f3c626c6345ea4adb5b224fa48d33608
andrew-stebbing <andrewstebbing@yahoo.co.uk>
2015-08-09 01:28:46 +0100

    Replaces the 4 `axis()` functions with a single argument list which is then reused.
    Each plot uses the same bespoke x-axis. A single 'list' of attributes is defined and the 'axis()' call for each plot is executed using `do.call(axis, x_axis_args)`

--- plot4.R
+++ plot4.R
@@ -27,7 +27,7 @@ d <- data[data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"), ]
 
 # Initial set-up:
 # Convert columns "Global_active_power, Global_reactive_power, Voltage and 
-# Sub_metering 1:3 to numbers
+# Sub_metering 1:3 from characters to numbers
 # These are found in columns 3:9
 d[, 3:9] <- lapply(d[, 3:9], as.numeric)
 
@@ -39,27 +39,31 @@ d[, 3:9] <- lapply(d[, 3:9], as.numeric)
 # Combine 4 plots on the same graphics device
 # arrangement is 2 X 2 grid ordered by column. 
 
+# the same bespoke x axis is used for all 4 plots
+# define a 'list' of attributes to pass to `axis()` just once and reuse using `do.call()`.
+x_axis_args <- list(1, at = c( 1, grep("2007-02-02", d$Date)[1], length(d$Date)), labels = c("Thurs", "Fri", "Sat"))
+
 par(mfcol = c(2,2), bg = "white")
 
 # Plot top left (same as plot2.png)
 plot(d$Global_active_power, type = "l", xaxt = "n", xlab = "", ylab = "Global Active Power")
-axis(1, at = c( 1, grep("2007-02-02", d$Date)[1], length(d$Date)), labels = c("Thurs", "Fri", "Sat"))
+do.call(axis, x_axis_args)
 
 # Plot bottom left (same as plot3.png) but with no border around the legend
 plot(d$Sub_metering_1, type = "n", xaxt = "n", xlab = "", ylab = "Energy sub metering")
 points(d$Sub_metering_1, type = "l", col = "black")
 points(d$Sub_metering_2, type = "l", col = "red")
 points(d$Sub_metering_3, type = "l", col = "blue")
-axis(1, at = c( 1, grep("2007-02-02", d$Date)[1], length(d$Date)), labels = c("Thurs", "Fri", "Sat"))
+do.call(axis, x_axis_args)
 legend("topright", lty = 1, bty = "n", col = c("black", "red", "blue"), legend = c("Sub_metering_1 ", "Sub_metering_2 ", "Sub_metering_3 "))
 
 # Plot top right
 plot(d$Voltage, type = "l", xaxt = "n", xlab = "datetime", ylab = "Voltage")
-axis(1, at = c( 1, grep("2007-02-02", d$Date)[1], length(d$Date)), labels = c("Thurs", "Fri", "Sat"))
+do.call(axis, x_axis_args)
 
 # Plot bottom right
 plot(d$Global_reactive_power, type = "l", xaxt = "n", xlab = "datetime", ylab = "Global_reactive_power")
-axis(1, at = c( 1, grep("2007-02-02", d$Date)[1], length(d$Date)), labels = c("Thurs", "Fri", "Sat"))
+do.call(axis, x_axis_args)
 
 # -----------------------------------------------
 
